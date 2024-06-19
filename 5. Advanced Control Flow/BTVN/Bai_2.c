1c0957b4d74e58b72d4a4cc67517960a0417ff46
zincVTD <thanhdanhldt@gmail.com>
2024-06-19 13:51:37 +0700

    Complete BTVN 2 and update Examples

--- 5. Advanced Control Flow/BTVN/Bai_2.c
+++ 5. Advanced Control Flow/BTVN/Bai_2.c
@@ -3,66 +3,75 @@
 
 jmp_buf buf;
 const char *errorCode;
+enum ErrorCodes
+{
+    NO_ERROR,
+    FILE_ERROR,
+    NETWORK_ERROR,
+    CALCULATION_ERROR
+};
+int bufStorage;
+
+#define TRY if ((bufStorage = setjmp(buf)) == NO_ERROR)
+#define CATCH(x) else if (bufStorage == x)
 #define THROW(x, y) \
     y;              \
     longjmp(buf, x);
 
-/*double divide(int a, int b) // Calculate the quotient of a and b (phep chia)
+void readFile()
 {
-    if (b == 0)
+    printf("Reading file...\n");
+    if (0) // Modify the condition to determine wether there is a error or not
     {
-        errorCode = THROW(1, "Error: Cannot be divide by 0");
+        errorCode = THROW(FILE_ERROR, "Error: File not found");
     }
-    return (double)a / b;
+    else
+        printf("Read complete\n");
 }
 
-double squareRoot(int a) // Calculate the square root of an integer (can bac 2)
+void networkOperation()
 {
-    if (a < 0)
+    printf("Connecting...\n");
+    if (1) // Modify the condition to determine wether there is a error or not
     {
-        errorCode = THROW(2, "Error: Cannot square root a negative number");
+        errorCode = THROW(NETWORK_ERROR, "Error: Can't connect to the network");
     }
-    double root = (double)a;
-    double x = 0;
-    while (root != x)
-    {
-        x = root;
-        root = 0.5 * (root + a / root);
-    }
-    return root;
+    else
+        printf("Network connected\n");
 }
 
-int factorial(int a) // Calculate the factorial of an integer (giai thua)
+void calculateData()
 {
-    if (a < 0)
+    printf("Calculating data...\n");
+    if (1) // Modify the condition to determine wether there is a error or not
     {
-        errorCode = THROW(3, "Error: Cannot factorial a negative number");
+        errorCode = THROW(CALCULATION_ERROR, "Error: Can't calculate the given data");
     }
-    int result = 1;
-    while (a != 0)
-    {
-        result *= a;
-        a--;
-    }
-    return result;
-}*/
+    else
+        printf("Data calculated\n");
+}
 
 int main()
 {
-    // Inputs
-    /*int a = 10;
-    int b = -1;
-
-    // Output
-    if (setjmp(buf) == 0)
+    TRY
     {
-        printf("%lf\n", divide(a, b));
-        // printf("%lf\n", squareRoot(b));
-        // printf("%d\n", factorial(a));
+        readFile();
+        networkOperation();
+        calculateData();
     }
-    else
+    CATCH(FILE_ERROR)
     {
         printf("%s\n", errorCode);
-    }*/
+    }
+    CATCH(NETWORK_ERROR)
+    {
+        printf("%s\n", errorCode);
+    }
+    CATCH(CALCULATION_ERROR)
+    {
+        printf("%s\n", errorCode);
+    }
+    // Finish statement
+    printf("Complete");
     return 0;
 }
