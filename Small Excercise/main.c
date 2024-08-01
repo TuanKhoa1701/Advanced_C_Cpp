b0d3a72314bbc5697eaf70a277be7cb6144efd8c
zincVTD <thanhdanhldt@gmail.com>
2024-08-01 20:08:20 +0700

    Just begin not done anything

---
+++ Small Excercise/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Home
+{
+    int YearBuilt;
+    int Landsize;
+    char Postcode[7];
+    int Price;
+    char Seller[10];
+} Home;
+
+void readData(const char *filename)
+{
+}
+
+int main()
+{
+}
